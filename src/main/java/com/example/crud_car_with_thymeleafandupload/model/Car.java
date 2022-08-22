f9b2310c085e06badef2491e69f5ec7fbe80fbd9
hongquan16101995 <66354589+hongquan16101995@users.noreply.github.com>
2022-08-22 14:05:44 +0700

    refactor

--- src/main/java/com/example/crud_car_with_thymeleafandupload/model/Car.java
+++ src/main/java/com/example/crud_car_with_thymeleafandupload/model/Car.java
@@ -34,16 +34,32 @@ public class Car {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getColor() {
         return color;
     }
 
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public double getPrice() {
         return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public String getImageUrl() {
